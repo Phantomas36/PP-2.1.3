2e07ef428ef544b23ff654e0172710de4152c141
ilyat <ilya.trubchaninov@yandex.ru>
2023-04-15 17:40:00 +0300

    Sdelano

---
+++ src/main/java/App.java
@@ -0,0 +1,21 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean =
+                (HelloWorld) applicationContext.getBean("helloworld", HelloWorld.class);
+        HelloWorld bean2 =
+                applicationContext.getBean("helloworld", HelloWorld.class);
+        Cat cat =
+                applicationContext.getBean("cat", Cat.class);
+        Cat cat2 =
+                applicationContext.getBean("cat", Cat.class);
+        System.out.println(bean == bean2);
+        System.out.println(cat == cat2);
+    }
+
+}
